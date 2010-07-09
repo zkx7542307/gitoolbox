7ad44d5d077b21804ecfc57431f7a5757848addd
xh0217 <xh0217@55ce492f-cb44-73c5-b190-8d40ae855696>
2010-07-09 16:57:02 +0000

    K2dfa now accepts filename and k

--- Beta Version 1.1/KBuilder.m
+++ Beta Version 1.1/KBuilder.m
@@ -1,3 +1,34 @@
+% Author: Hasan Ibne Akram, Huang Xiao
+% Munich University of Technology
+% Web: http://www.sec.in.tum.de/hasan-akram/
+% Email: hasan.akram@sec.in.tum.de
+%          huang.xiao@mytum.de
+% Copyright © 2010
+% 
+% This is a beta version
+% 
+% DISCLAIMER OF WARRANTY
+% This source code is provided "as is" and without warranties
+% as to performance or merchantability. The author and/or 
+% distributors of this source code may have made statements 
+% about this source code. Any such statements do not constitute 
+% warranties and shall not be relied on by the user in deciding
+% whether to use this source code.
+% 
+% This source code is provided without any express or implied
+% warranties whatsoever. Because of the diversity of conditions
+% and hardware under which this source code may be used, no
+% warranty of fitness for a particular purpose is offered. The 
+% user is advised to test the source code thoroughly before relying
+% on it. The user must assume the entire risk of using the source code.
+% 
+% -------------------------------------------------
+% Read samples from file and convert it into a Ktestable language
+% Input: filename,the path of sample file
+%        k, the learning parameter k
+% Output: return a K-testable language
+% see also KSET, K2dfa
+
 function kset = KBuilder(filename, k)
 %KBUILDER Summary of this function goes here
 %   Detailed explanation goes here
