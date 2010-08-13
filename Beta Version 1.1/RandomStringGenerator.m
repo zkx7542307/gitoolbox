c67a3e3f183e12383b077dfe598c63f170a64e3c
xh0217 <xh0217@55ce492f-cb44-73c5-b190-8d40ae855696>
2010-08-13 09:38:37 +0000

    del ver 1.1

--- Beta Version 1.1/RandomStringGenerator.m
+++
@@ -1,8 +0,0 @@
-function String = RandomStringGenerator(LetterStore, maxlength)
-
-    %LetterStore = '10';
-
-    n = ceil(maxlength*rand(1));
-
-    String = LetterStore(ceil(length(LetterStore).*rand(1,n)));
-end 
